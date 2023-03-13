a65dc8e550006c8c8a878b8ead9fd7899c57721c
VshivcevA <vshivcev.alexandr@gmail.com>
2023-03-13 11:09:15 +0200

    await

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-const template = document.querySelector('#template')
-  .content.querySelector('.card-list__item')
-const cardList = document.querySelector('.card-list')
-const input = document.getElementById('search')
-const searchButton = document.getElementById('search-button')
+const template = document
+  .querySelector("#template")
+  .content.querySelector(".card-list__item");
+const cardList = document.querySelector(".card-list");
+const input = document.getElementById("search");
+const searchButton = document.getElementById("search-button");
 
-window.addEventListener('DOMContentLoaded', () => {
-  fetch('https://jsonplaceholder.typicode.com/posts/?_start=0&_limit=20')
-    .then((response) => {
-      return response.json();
-    })
-    .then((dataArray) => {
-      search(dataArray)
-    })
+// window.addEventListener("DOMContentLoaded", () => {
+//   fetch("https://jsonplaceholder.typicode.com/posts/?_start=0&_limit=20")
+//     .then((response) => response.json())
+//     .then((dataArray) => search(dataArray));
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+  (async function get() {
+    let response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/?_start=0&_limit=20"
+    );
+    let data = await response.json();
+    search(data);
+  })();
 });
 
 const getCheckbox = (item) => {
-  const card = item.querySelector('.card')
-  const checkbox = item.querySelector('#checkbox')
-  checkbox.addEventListener('change', () => {
+  const card = item.querySelector(".card");
+  const checkbox = item.querySelector("#checkbox");
+  checkbox.addEventListener("change", () => {
     if (checkbox.checked) {
-      card.classList.add('checked');
+      card.classList.add("checked");
     } else {
-      card.classList.remove('checked');
+      card.classList.remove("checked");
     }
-  })
-}
+  });
+};
 
 const search = (dataArray) => {
-  input.value = ''
-  function renderList(dataArray,cardList){
-    cardList.innerHTML='';
-    dataArray.forEach(data => {
+  input.value = "";
+  function renderList(dataArray, cardList) {
+    cardList.innerHTML = "";
+    dataArray.forEach((data) => {
       const item = template.cloneNode(true);
-      item.querySelector('.card__title').textContent = data.title;
-      item.querySelector('.card__text').textContent = data.body;
-      getCheckbox(item)
-      cardList.appendChild(item)
-    })
+      item.querySelector(".card__title").textContent = data.title;
+      item.querySelector(".card__text").textContent = data.body;
+      getCheckbox(item);
+      cardList.appendChild(item);
+    });
   }
-  renderList(dataArray,cardList)
+  renderList(dataArray, cardList);
 
-  function filter(val,dataArray){
-    return dataArray.filter(data=>(~data.title.indexOf(val)))
+  function filter(val, dataArray) {
+    return dataArray.filter((data) => ~data.title.indexOf(val));
   }
 
-  searchButton.addEventListener('click', () => {
-    renderList(filter(input.value, dataArray),cardList)
-    location.href = '#search/' + input.value
-    input.value = '';
-  })
-}
+  searchButton.addEventListener("click", () => {
+    renderList(filter(input.value, dataArray), cardList);
+    location.href = "#search/" + input.value;
+    input.value = "";
+  });
+};
